2ab237f68788d802d0d613b0d81bd1e3696b7ef1
aniket awati <aniket@rainingclouds.com>
2015-02-22 09:28:02 +0530

    Use format.Node to write AST to file instead of string manipulation

--- easytags.go
+++ easytags.go
@@ -4,10 +4,10 @@ import (
 	"bufio"
 	"fmt"
 	"go/ast"
+	"go/format"
 	"go/parser"
 	"go/token"
 	"os"
-	"os/exec"
 	"strings"
 	"unicode"
 )
@@ -17,15 +17,10 @@ func main() {
 	fset := token.NewFileSet() // positions are relative to fset
 	args := os.Args[1:]
 	if len(args) < 2 {
-		fmt.Println("Usage : easytags {file_name} {tag_name} {debug (true/false)} \n example: easytags file.go json true")
+		fmt.Println("Usage : easytags {file_name} {tag_name} \n example: easytags file.go json true")
 		return
 	}
-	debug := false
-	if len(args) == 3 {
-		if args[2] == "true" {
-			debug = true
-		}
-	}
+
 	tagName := args[1]
 	// Parse the file given in arguments
 	f, err := parser.ParseFile(fset, args[0], nil, parser.ParseComments)
@@ -34,12 +29,7 @@ func main() {
 		fmt.Println(err)
 		return
 	}
-	// read entire source file as a slice of lines
-	lines, err := readLines(args[0])
-	if err != nil {
-		fmt.Printf("Error reading file %v \n ", err)
-		return
-	}
+
 	// range over the objects in the scope of this generated AST and check for StructType. Then range over fields
 	// contained in that struct.
 	for _, d := range f.Scope.Objects {
@@ -55,74 +45,41 @@ func main() {
 				continue
 			}
 			for _, field := range x.Fields.List {
-				line := fset.File(field.Pos()).Line(field.Pos())
-				line = line - 1
 				if len(field.Names) == 0 {
 					continue
 				}
 				// if tag for field doesn't exists, create one
 				if field.Tag == nil {
 					name := field.Names[0].String()
-					if debug {
-						fmt.Printf("Replacing line %s \n ", lines[line])
-					}
-					lines[line] = fmt.Sprintf("%s %v `%s:\"%s\"`", name, field.Type, tagName, ToSnake(name))
-					if debug {
-						fmt.Printf("By line : %s \n", lines[line])
-					}
+					field.Tag = &ast.BasicLit{}
+					field.Tag.ValuePos = field.Type.Pos() + 1
+					field.Tag.Kind = token.STRING
+					field.Tag.Value = fmt.Sprintf("`%s:\"%s\"`", tagName, ToSnake(name))
 				} else if !strings.Contains(field.Tag.Value, fmt.Sprintf("%s:", tagName)) {
 					// if tag exists, but doesn't contain target tag
 					name := field.Names[0].String()
-					if debug {
-						fmt.Printf("Replacing line %s \n ", lines[line])
-					}
-					lines[line] = fmt.Sprintf("%s %v `%s:\"%s\" %s`", name, field.Type, tagName, ToSnake(name), strings.Replace(field.Tag.Value, "`", "", 2))
-					if debug {
-						fmt.Printf("By line : %s \n", lines[line])
-					}
-
+					field.Tag.ValuePos = field.Type.Pos() + 1
+					field.Tag.Kind = token.STRING
+					field.Tag.Value = fmt.Sprintf("`%s:\"%s\" %s`", tagName, ToSnake(name), strings.Replace(field.Tag.Value, "`", "", 2))
 				}
-
 			}
 		}
 	}
-	// overwrite the file with modified version of lines.
-	writeLines(lines, args[0])
-	cmd := exec.Command("go", "fmt", args[0])
-	cmd.Run()
-}
 
-// readLines reads a whole file into memory
-// and returns a slice of its lines.
-// original source : http://stackoverflow.com/questions/5884154/golang-read-text-file-into-string-array-and-write
-func readLines(path string) ([]string, error) {
-	file, err := os.Open(path)
+	// overwrite the file with modified version of ast.
+	write, err := os.Create(args[0])
 	if err != nil {
-		return nil, err
+		fmt.Printf("Error opening file %v", err)
+		return
 	}
-	defer file.Close()
-
-	var lines []string
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		lines = append(lines, scanner.Text())
-	}
-	return lines, scanner.Err()
-}
-
-// writeLines writes the lines to the given file.
-func writeLines(lines []string, path string) error {
-	file, err := os.Create(path)
+	defer write.Close()
+	w := bufio.NewWriter(write)
+	err = format.Node(w, fset, f)
 	if err != nil {
-		return err
+		fmt.Printf("Error formating file", err)
+		return
 	}
-	defer file.Close()
-
-	w := bufio.NewWriter(file)
-	for _, line := range lines {
-		fmt.Fprintln(w, line)
-	}
-	return w.Flush()
+	w.Flush()
 }
 
 // ToSnake convert the given string to snake case following the Golang format:
@@ -139,6 +96,5 @@ func ToSnake(in string) string {
 		}
 		out = append(out, unicode.ToLower(runes[i]))
 	}
-
 	return string(out)
 }
