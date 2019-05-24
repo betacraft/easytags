c83927f0aa3a08c0fbb86d19d0d6b636bb0648a6
Alex Tsokurov <me@ximik.net>
2019-05-25 00:15:28 +0200

    different cases support

--- README.md
+++ README.md
@@ -3,24 +3,24 @@ Easy json/xml Tag generation tool for golang
 
 [![Build Status](https://travis-ci.org/betacraft/easytags.svg?branch=master)](https://travis-ci.org/rainingclouds/easytags)
 
-We generally write Field names in CamelCase and we generally want them to be in snake case when marshalled to json/xml/sql etc. We use tags for this purpose. But it is a repeatative process which should be automated. 
+We generally write Field names in CamelCase (aka pascal case) and we generally want them to be in snake case (camel and pascal case are supported as well) when marshalled to json/xml/sql etc. We use tags for this purpose. But it is a repeatative process which should be automated.
 
 usage :
 
-> easytags {file_name} {tag_name_1, tag_name_2} 
->example: easytags file.go 
+> easytags {file_name} {tag_name_1:case_1, tag_name_2:case_2}
 
-You can also use this with go generate 
-For example - In your source file, write following line 
+> example: easytags file.go
 
->go:generate easytags $GOFILE json,xml,sql
+You can also use this with go generate
+For example - In your source file, write following line
+
+> go:generate easytags $GOFILE json,xml,sql
 
 And run
->go generate
+> go generate
 
-This will go through all the struct declarations in your source files, and add corresponding json/xml/sql tags with field name changed to snake case. If you have already written tag with "-" value , this tool will not change that tag.
+This will go through all the struct declarations in your source files, and add corresponding json/xml/sql tags with field name changed to snake case. If you have already written tag with "-" value, this tool will not change that tag.
 
 Now supports Go modules.
 
 ![Screencast with Go Generate](https://media.giphy.com/media/26n6G34sQ4hV8HMgo/giphy.gif)
-
--- easytags_test.go
+++ easytags_test.go
@@ -14,13 +14,13 @@ func TestGenerateTags(t *testing.T) {
 		t.Errorf("Error reading file %v", err)
 	}
 	defer ioutil.WriteFile("testfile.go", testCode, 0644)
-	GenerateTags("testfile.go", []string{"json"}, false)
+	GenerateTags("testfile.go", []*TagOpt{&TagOpt{"json", "snake"}}, false)
 	fset := token.NewFileSet()
 	f, err := parser.ParseFile(fset, "testfile.go", nil, parser.ParseComments)
 	if err != nil {
 		t.Errorf("Error parsing generated file %v", err)
 		genFile, _ := ioutil.ReadFile("testfile.go")
-		t.Errorf("\n%s",genFile)
+		t.Errorf("\n%s", genFile)
 		return
 	}
 
@@ -76,7 +76,7 @@ func TestGenerateTags_Multiple(t *testing.T) {
 		t.Errorf("Error reading file %v", err)
 	}
 	defer ioutil.WriteFile("testfile.go", testCode, 0644)
-	GenerateTags("testfile.go", []string{"json", "xml"}, false)
+	GenerateTags("testfile.go", []*TagOpt{&TagOpt{"json", "snake"}, &TagOpt{"xml", "snake"}}, false)
 	fset := token.NewFileSet()
 	f, err := parser.ParseFile(fset, "testfile.go", nil, parser.ParseComments)
 	if err != nil {
@@ -130,13 +130,62 @@ func TestGenerateTags_Multiple(t *testing.T) {
 	}
 }
 
+func TestGenerateTags_PascalCase(t *testing.T) {
+	testCode, err := ioutil.ReadFile("testfile.go")
+	if err != nil {
+		t.Errorf("Error reading file %v", err)
+	}
+	defer ioutil.WriteFile("testfile.go", testCode, 0644)
+	GenerateTags("testfile.go", []*TagOpt{&TagOpt{"json", "camel"}}, false)
+	fset := token.NewFileSet()
+	f, err := parser.ParseFile(fset, "testfile.go", nil, parser.ParseComments)
+	if err != nil {
+		t.Errorf("Error parsing generated file %v", err)
+		genFile, _ := ioutil.ReadFile("testfile.go")
+		t.Errorf("\n%s", genFile)
+		return
+	}
+
+	for _, d := range f.Scope.Objects {
+		if d.Kind != ast.Typ {
+			continue
+		}
+		ts, ok := d.Decl.(*ast.TypeSpec)
+		if !ok {
+			t.Errorf("Unknown type without TypeSec: %v", d)
+			return
+		}
+
+		x, ok := ts.Type.(*ast.StructType)
+		if !ok {
+			continue
+		}
+		for _, field := range x.Fields.List {
+			if len(field.Names) == 0 {
+				if field.Tag != nil {
+					t.Errorf("Embedded struct shouldn't be added a tag - %s", field.Tag.Value)
+				}
+				continue
+			}
+			name := field.Names[0].String()
+			if name == "TestField2" {
+				if field.Tag == nil {
+					t.Error("Tag should be generated for TestFiled2")
+				} else if field.Tag.Value != "`json:\"testField2\"`" {
+					t.Error("Camel tag should be generated for TestField2")
+				}
+			}
+		}
+	}
+}
+
 func TestGenerateTags_RemoveAll(t *testing.T) {
 	testCode, err := ioutil.ReadFile("testfile.go")
 	if err != nil {
 		t.Errorf("Error reading file %v", err)
 	}
 	defer ioutil.WriteFile("testfile.go", testCode, 0644)
-	GenerateTags("testfile.go", []string{}, true)
+	GenerateTags("testfile.go", []*TagOpt{}, true)
 	fset := token.NewFileSet()
 	f, err := parser.ParseFile(fset, "testfile.go", nil, parser.ParseComments)
 	if err != nil {
@@ -177,4 +226,30 @@ func TestGenerateTags_RemoveAll(t *testing.T) {
 			}
 		}
 	}
+}
+
+func TestToSnake(t *testing.T) {
+	test := func(in, out string) {
+		r := ToSnake(in)
+		if r != out {
+			t.Errorf("%s in snake_case should be %s, instead found %s", in, out, r)
+		}
+	}
+	test("A", "a")
+	test("ID", "id")
+	test("UserID", "user_id")
+	test("CSRFToken", "csrf_token")
+}
+
+func TestToCamel(t *testing.T) {
+	test := func(in, out string) {
+		r := ToCamel(in)
+		if r != out {
+			t.Errorf("%s in lowerCamelCase should be %s, instead found %s", in, out, r)
+		}
+	}
+	test("A", "a")
+	test("ID", "id")
+	test("UserID", "userID")
+	test("CSRFToken", "csrfToken")
 }
